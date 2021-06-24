611dd5f66660b5f86d7e3fd95daf301b35bc87d3
Minh Chu <scsilver94@gmail.com>
2021-06-24 11:32:31 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 # Install ubuntu package
-sudo apt install curl git zsh htop xclip ripgrep
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install curl git zsh htop xclip ripgrep tmux
 
 # Install nvim
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
@@ -13,6 +10,7 @@ sudo mv nvim.appimage /usr/local/bin/nvim
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ${USER}
+rm get-docker.sh
 # logout your computer then login again
 
 # Install docker-compose
@@ -22,23 +20,44 @@ sudo chmod +x /usr/local/bin/docker-compose
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
+# Install node
+nvm install --lts
+
 # Install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# Install nerd-fonts
+# Clone nerd-fonts
 git clone https://github.com/ryanoasis/nerd-fonts ~/codes/nerd-fonts --depth=1
 
+# Install nerd-fonts
+cd ~/codes/nerd-fonts
+./install.sh SourceCodePro
+
 # Install devilbox
-git clone https://github.com/cytopia/devilbox --depth=1
+git clone https://github.com/cytopia/devilbox ~/codes/devilbox --depth=1
 
 # Install space-vim
 bash <(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)
-
-# Install palenight theme
-bash -c  "$(wget -qO- https://git.io/vQgMr)"
 
 # Install ibus bamboo
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
 sudo apt-get install ibus-bamboo
 ibus restart
+
+# Install palenight theme
+mkdir -p "$HOME/src"
+cd "$HOME/src"
+git clone https://github.com/Mayccoll/Gogh.git gogh
+cd gogh/themes
+export TERMINAL=gnome-terminal
+./palenight.sh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy config manually
+cp .spacevim ~/.spacevim
+cp .tmux.conf ~/.tmux.conf
+cp .zshrc ~/.zshrc
+cp .gitconfig ~/.gitconfig
