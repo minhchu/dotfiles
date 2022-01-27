d8d3b4c2d8cca6d89f11f8cf1888376f9c900133
Minh Chu <scsilver94@gmail.com>
2022-01-27 11:23:44 +0700

    Remove unused packages

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
 # Install node
+source ~/.nvm/nvm.sh
 nvm install --lts
 
 # Install tmux plugin manager
@@ -36,22 +37,11 @@ cd ~/codes/nerd-fonts
 # Install devilbox
 git clone https://github.com/cytopia/devilbox ~/codes/devilbox --depth=1
 
-# Install space-vim
-bash <(curl -fsSL https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)
-
 # Install ibus bamboo
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
 sudo apt-get install ibus-bamboo
 ibus restart
-
-# Install palenight theme
-mkdir -p "$HOME/src"
-cd "$HOME/src"
-git clone https://github.com/Mayccoll/Gogh.git gogh
-cd gogh/themes
-export TERMINAL=gnome-terminal
-./palenight.sh
 
 # Install github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -59,17 +49,10 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt update
 sudo apt install gh
 
-# Install pop-os shell
-npm install -g typescript
-git clone https://github.com/pop-os/shell.git ~/codes/pop-os-shell
-cd ~/codes/pop-os-shell
-make local-install
-
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Copy config manually
-cp .spacevim ~/.spacevim
 cp .tmux.conf ~/.tmux.conf
 cp .zshrc ~/.zshrc
 cp .gitconfig ~/.gitconfig
